5a16036591dfaeecfc3cf8d4cda3559fcae45b76
WillFrancois <williamjcfrancois@gmail.com>
2024-09-03 18:51:01 -0400

    created workoutsession api calls

--- WorkoutSessions.py
+++ WorkoutSessions.py
@@ -12,8 +12,16 @@ class WorkoutSchema(Schema):
     s_id = fields.Integer(required=True, strict=True)
     m_id = fields.Integer(required=True, strict=True) #This is a foreign key that must connect with another table
     s_date = fields.Date()
-    s_time = fields.String(validate=validate.Length(max=255))
+    s_time = fields.Time(format="%H:%M")
     activity = fields.String(validate=validate.Length(max=255))
 
     class Meta:
         fields = ("s_id", "m_id", "s_date", "s_time", "activity")
+
+class WorkoutUpdateSchema(Schema):
+    s_date = fields.Date()
+    s_time = fields.Time(format="%H:%M")
+    activity = fields.String(validate=validate.Length(max=255))
+
+    class Meta:
+        fields = ("s_date", "s_time", "activity")
--- app.py
+++ app.py
@@ -51,12 +51,10 @@ def view_member(id):
 @app.route("/members/<int:id>", methods=["PUT"])
 def update_member(id):
     try:
-        #Receive and validate member information
         req = request.get_json()
         member_schema = Members.MemberUpdateSchema()
         member = Members.MemberUpdateSchema.load(member_schema, data=req)
 
-        #Append to database
         cur = conn.cursor()
         cur.execute("update Members set name = %s, age = %s where id = %s", [member["name"], member["age"], id])
         conn.commit()
@@ -76,3 +74,67 @@ def remove_member(id):
         return "Deletion successful!"
     except Exception as e:
         return f"Could not find user. Incorrect ID provided or database is unavailable."
+
+
+#Task 3 (Session CRUD starts here!)
+#NOTE: Assignment did not specify creation of deletion function like the previous task, so one was not made
+@app.route("/sessions", methods=["POST"])
+def create_session():
+    try:
+        req = request.get_json()
+        print(req)
+        workout_schema = WorkoutSessions.WorkoutSchema()
+        session = WorkoutSessions.WorkoutSchema.load(workout_schema, data=req)
+
+        cur = conn.cursor()
+        cur.execute("insert into WorkoutSessions(session_id, member_id, session_date, session_time, activity) values(%s, %s, %s, %s, %s)", [session["s_id"], session["m_id"], session["s_date"], session["s_time"], session["activity"]])
+        conn.commit()
+        cur.close()
+        return "Success, session added."
+
+    except Exception as e:
+        return f"Could not complete request. {e}"
+
+@app.route("/sessions/<int:id>", methods=['GET'])
+def view_session(id):
+    try:
+        cur = conn.cursor()
+        cur.execute("select * from WorkoutSessions where session_id = %s", [id])
+        data = cur.fetchone()
+        cur.close()
+        return jsonify({'session_id': data[0], 'member_id': data[1], 'session_date': data[2], 'session_time': data[3], 'activity': data[4]})
+    except Exception as e:
+        return f"Could not find session. Incorrect ID provided or database is unavailable."
+
+
+@app.route("/sessions/<int:id>", methods=["PUT"])
+def update_session(id):
+    try:
+        req = request.get_json()
+        workout_schema = WorkoutSessions.WorkoutUpdateSchema()
+        session = WorkoutSessions.WorkoutUpdateSchema.load(workout_schema, data=req)
+
+        cur = conn.cursor()
+        cur.execute("update WorkoutSessions set session_date = %s, session_time = %s, activity = %s where session_id = %s", [session["s_date"], session["s_time"], session["activity"], id])
+        conn.commit()
+        cur.close()
+        return "Success, session updated."
+
+    except Exception as e:
+        return f"Could not complete request. {e}"
+
+@app.route("/sessions/member/<int:m_id>", methods=["GET"])
+def users_sessions(m_id):
+    try:
+        cur = conn.cursor()
+        cur.execute("select * from WorkoutSessions inner join Members where Members.id=WorkoutSessions.member_id and member_id = %s", [m_id])
+        data = cur.fetchall()
+        cur.close()
+
+        return_value = {}
+        for point in range(0, len(data)):
+            return_value[point] = {"Session_ID": data[point][0], "Member_ID": data[point][1], "Session_Date": data[point][2], "Session_Time": data[point][3], "Activity": data[point][4], "Member_Name": data[point][6]}
+        return jsonify(return_value)
+
+    except Exception as e:
+        return f"Could not complete request. {e}"
